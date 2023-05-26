b0f721c2e1ca8f36bca7de6aebec4a59f18410c5
marcusyim <marcusyim2004@gmail.com>
2023-05-25 19:58:11 -0700

    added create a design

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -16,3 +16,20 @@ CREATE TABLE Vaccines (
     Doses int,
     PRIMARY KEY (Name)
 );
+
+CREATE TABLE Patients (
+	Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointments (
+    apID int PRIMARY KEY,
+    cUsername varchar(255),
+    pUsername varchar(255),
+    vName varchar(255),
+    FOREIGN KEY(cUsername) REFERENCES Caregivers(username),
+    FOREIGN KEY(pUsername) REFERENCES Patients(username),
+    FOREIGN KEY(vName) REFERENCES Vaccines(name)
+);
